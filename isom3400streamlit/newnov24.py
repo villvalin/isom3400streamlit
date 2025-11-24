32110884db5e505ae5b4ca7dcc7d9d885888877b
villvalin <vsac@connect.ust.hk>
2025-11-24 14:08:08 +0800

    Create newnov24.py

---
+++ isom3400streamlit/newnov24.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.title('Hello, Students!')
+st.write('This is your Python Programming course.')
+
+with st.sidebar:
+    selected=option_menu(
+        menu_title = "Menu",
+        options = ["Home", "About", "Contact"],
+        icons = ["house", "cloud-upload", "list-task"],
+        menu_icon= "emoji-smile-fill",
+        default_index=0,
+    )
+
+if selected == "Home":
+    st.title(f"Welcome to the {selected} page.")
+
+if selected == "About":
+    st.title(f"Welcome to the {selected} page.")
+
+if selected == "Contact":
+    st.title(f"Welcome to the {selected} page.")
