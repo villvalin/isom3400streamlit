ca334f2b0cb343438be65147f5ba911d3adae6ee
villvalin <vsac@connect.ust.hk>
2025-11-24 14:32:45 +0800

    Update newwnov24.py

--- isom3400streamlit/newwnov24.py
+++ isom3400streamlit/newwnov24.py
@@ -5,7 +5,7 @@ import os
 # Get the current working directory
 current_directory = os.getcwd()
 # Define the file path
-file_path = os.path.join(current_directory, 'winequality-red.csv')
+file_path = os.path.join(current_directory, 'isom3400streamlit/winequality-red.csv')
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(file_path, delimiter=';')
