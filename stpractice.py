8f8112ff232c208f16b2efc023f2f738bbfd62ee
villvalin <vsac@connect.ust.hk>
2025-12-07 00:29:32 +0800

    Create stpractice.py

---
+++ stpractice.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.title("Retail Business Dashboardf")
+st.header("Manager Input Selection")
+st.write("Please enter the monthly sales target and select the region")
+
+sales=st.number_input("Enter monthly sales target (in USD))
+region=st.selectbox("Select region:",["North","South","East"])
+
+if st.button("Submit"):
+  st.success(f"Your target is {sales} in {region})
+      
