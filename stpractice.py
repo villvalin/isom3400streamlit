0ec5b689a5f5266fb0dce9f9f1c752033290ba14
villvalin <vsac@connect.ust.hk>
2025-12-07 00:31:01 +0800

    Update stpractice.py

--- stpractice.py
+++ stpractice.py
@@ -7,7 +7,7 @@ st.title("Retail Business Dashboardf")
 st.header("Manager Input Selection")
 st.write("Please enter the monthly sales target and select the region")
 
-sales=st.number_input("Enter monthly sales target (in USD))
+sales=st.number_input("Enter monthly sales target (in USD)")
 region=st.selectbox("Select region:",["North","South","East"])
 
 if st.button("Submit"):
