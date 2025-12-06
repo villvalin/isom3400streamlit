820a2b3bb8d6035a327cb11bb454bd073398f888
villvalin <vsac@connect.ust.hk>
2025-12-07 00:32:03 +0800

    Update stpractice.py

--- stpractice.py
+++ stpractice.py
@@ -11,5 +11,5 @@ sales=st.number_input("Enter monthly sales target (in USD)")
 region=st.selectbox("Select region:",["North","South","East"])
 
 if st.button("Submit"):
-  st.success(f"Your target is {sales} in {region})
+  st.success(f"Your target is {sales} in {region}")
       
